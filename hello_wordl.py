a66dee4ba9250fb346a8b0c0fcc300569ede3906
Santiago Diaz <zantiago29@hotmail.com>
2017-06-22 04:47:13 +0000

    final commit

--- hello_wordl.py
+++ hello_wordl.py
@@ -4,6 +4,7 @@ from os import environ
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/hello")
 def say_hi():
     return render_template("base.html")
     
@@ -14,18 +15,8 @@ def hello_person(name):
 @app.route("/jedi/<firstname>/<lastname>")
 def jedi(firstname, lastname):
     jediname = lastname[0:3] + firstname[0:2]
-  
-    html = """
-        <h1>
-        Hello {} {}!
-        </h1>
-        <p>
-        Your jedi name is: {}
-        </p>
-        """
-    return html.format(firstname.title(), lastname.title(), jediname.title())
+    return render_template("jedi_template.html", jediname=jediname, firstname=firstname, lastname=lastname)
         
-
 if __name__ == "__main__":
     app.run(host=environ['IP'],
             port=int(environ['PORT']))
